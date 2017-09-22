d01fe99a2c72ce6691fcc2ed611cb86f67648de7
Sara <ssimon@thoughtworks.com>
2017-09-22 11:40:02 +0200

    Open a new tab on click and load react app

--- src/app.js
+++ src/app.js
@@ -2,5 +2,4 @@ import React from "react"
 import ReactDOM from "react-dom"
 import Login from "./login"
 
-console.log(document.getElementById('app'))
 ReactDOM.render(<h2>helloooooo</h2>, document.getElementById('app'));
