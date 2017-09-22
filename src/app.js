0c06228da1d566408a29049fb47e6a760aeb5c90
elegarpes <elegarpes@gmail.com>
2017-09-22 10:08:22 +0200

    First test with react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-alert('hola')
+import React from "react"
+import ReactDOM from "react-dom"
+import Login from "./login"
+
+console.log(document.getElementById('app'))
+ReactDOM.render(<h2>helloooooo</h2>, document.getElementById('app'));
